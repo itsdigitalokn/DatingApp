7fb06edabe5e5e6535e2bedae3959765c2a472cd
Olivier Knockaert <olivier.knockaert@itsdigital.be>
2021-05-03 19:43:35 +0200

    End of section 10

--- API/Startup.cs
+++ API/Startup.cs
@@ -64,7 +64,9 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyHeader().WithOrigins("https://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader()
+                .AllowAnyMethod()
+                .WithOrigins("https://localhost:4200"));
 
             app.UseAuthentication();
             app.UseAuthorization();
